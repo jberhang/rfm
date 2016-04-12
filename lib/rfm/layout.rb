cb6b48558c90dc3287678d72b19619eda785d7d9
Justin Berhang <justin@scratch.com>
2016-04-12 10:23:51 -0400

    add sleep before system error retry

--- lib/rfm/layout.rb
+++ lib/rfm/layout.rb
@@ -220,6 +220,7 @@ module Rfm
       rescue Rfm::Error::SystemError => e
         if system_error_retry_count < 10
           system_error_retry_count += 1
+          sleep 2
           retry
         else
           raise e
